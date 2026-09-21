70712d7db257558a805328645cf0ad36a71ea63a
Rounak Seth <sethrounak7@gmail.com>
2026-09-21 16:32:23 +0530

    Fix the description alignment

---
+++ client/src/ProtectedRoute.jsx
@@ -0,0 +1,21 @@
+import { useContext } from "react";
+import { UserContext } from "./context/AuthContext";
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ children }) => {
+    const { isLoggedIn, isCheckingAuth } = useContext(UserContext);
+
+    if (isCheckingAuth) {
+        return (
+            <div className="fixed inset-0 flex items-center justify-center">
+                <div className="h-10 w-10 animate-spin rounded-full border-4 border-gray-300 border-t-blue-600"></div>
+            </div>
+        );
+    }
+
+    return isLoggedIn
+        ? children
+        : <Navigate to="/" replace />;
+};
+
+export default ProtectedRoute;
